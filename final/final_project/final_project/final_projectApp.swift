f0099bc8b169732f9693b719c8d52cef07e9ba6f
yutsen47 <trina940325@gmail.com>
2026-06-08 17:37:30 +0800

    Add files via upload

---
+++ final/final_project/final_project/final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_projectApp.swift
+//  final_project
+//
+//  Created by tsen on 2026/6/8.
+//
+
+import SwiftUI
+
+@main
+struct final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
